922b82e891df44de2399164af6fc2ac904fbd266
Samuel Ernst <samuel.ernst@epfl.ch>
2021-01-24 14:01:22 +0100

    main loop timing + app struct

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,60 +1,79 @@
 
 #include "imgui_includes.cpp"
 
-
 #include <SFML/Graphics.hpp>
-#include <iostream>
+
+struct Application
+{
+	sf::RenderWindow *window;
+	sf::Clock update_clock;
+	sf::Time frame_duration;
+	sf::Time frame_target_duration;
+
+	sf::Clock debug_clock;
+};
+
+Application *global_app;
 
 int main()
 {
-    std::cout << "Hello World\n";
-
+	Application app;
+	app.window = new sf::RenderWindow(sf::VideoMode(800,600), "Hello");
+	app.frame_duration = sf::Time::Zero;
+	app.frame_target_duration = sf::seconds(1.f/60.f);
+	global_app = &app;
+	
 #ifdef DEBUG
-	std::cout << "Debug\n";
-#endif
-
-	sf::RenderWindow window(sf::VideoMode(800,600), "Hello");
-
-	sf::Clock update_clock;
-
-#ifdef DEBUG
-	ImGui::SFML::Init(window);
+	ImGui::SFML::Init(*app.window);
 #endif
 	
-	while(window.isOpen())
+	while(app.window->isOpen())
 	{
-		sf::Event event;
-		while(window.pollEvent(event))
+
+		app.frame_duration += app.update_clock.restart();
+		if(app.frame_duration > app.frame_target_duration)
 		{
+			app.frame_duration -= app.frame_target_duration; 
+		
+			sf::Event event;
+			while(app.window->pollEvent(event))
+			{
 #ifdef DEBUG
-			ImGui::SFML::ProcessEvent(event);
+				ImGui::SFML::ProcessEvent(event);
 #endif
-			if(event.type == sf::Event::Closed)
-				window.close();
-		}
+				if(event.type == sf::Event::Closed)
+					app.window->close();
+			}
 
 #ifdef DEBUG
-		ImGui::SFML::Update(window, update_clock.restart());
+			ImGui::SFML::Update(*app.window, app.update_clock.restart());
 #endif
 		
-		window.clear();
+			app.window->clear();
 
 #ifdef DEBUG
-		ImGui::ShowDemoWindow();
-		ImGui::Begin("Test");
-		ImGui::Text("Yolo");
-		ImGui::End();
+			ImGui::ShowDemoWindow();
+			ImGui::Begin("Debug Infos");
+			int debug_time = app.debug_clock.restart().asMilliseconds();
+			ImGui::Text("Frame duration: %ims (%.2ffps)", debug_time, 1e3/(float)debug_time);
+			ImGui::End();
 #endif
 
 #ifdef DEBUG
-		ImGui::SFML::Render(window);
+			ImGui::SFML::Render(*app.window);
 #endif
-		window.display();
+			app.window->display();
+
+		}
+		
 	}
 
 #ifdef DEBUG
 	ImGui::SFML::Shutdown();
 #endif
+
+
+	delete app.window;
 	
     return 0;
 }
